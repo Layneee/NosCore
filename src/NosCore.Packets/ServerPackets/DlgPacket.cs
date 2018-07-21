997703dba81f97160f1dc41a83d513fdfd8d6eb5
0Lucifer0 <erwan.joly@gmail.com>
2018-07-21 12:17:27 +1200

    add a test and fix #139

--- src/NosCore.Packets/ServerPackets/DlgPacket.cs
+++ src/NosCore.Packets/ServerPackets/DlgPacket.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 using NosCore.Core.Serializing;
+using NosCore.Packets.ClientPackets;
 
 namespace NosCore.Packets.ServerPackets
 {
     [PacketHeader("dlg")]
     public class DlgPacket : PacketDefinition
     {
-        [PacketIndex(0, SerializeToEnd = true)]
-        public string PacketContent { get; set; } //TODO: Review this
+        [PacketIndex(0, IsReturnPacket = true)]
+        public FinsPacket YesPacket { get; set; }
+
+        [PacketIndex(1, IsReturnPacket = true)]
+        public FinsPacket NoPacket { get; set; }
+
+        [PacketIndex(2, SerializeToEnd = true)]
+        public string Question { get; set; }
     }
 }
